36ef46c087770a6e4040634d3ebebcffc86abcbf
Karl Preisner <karl@sindri.app>
2023-11-22 15:20:03 +0000

    update python scripts for halo2 circuit_tutorials

--- circuit_tutorials/halo2/axiom-v0.3.0/float_radius/compile_and_prove.py
+++ circuit_tutorials/halo2/axiom-v0.3.0/float_radius/compile_and_prove.py
@@ -6,6 +6,7 @@ import os
 
 # NOTE: Provide your API Key here
 API_KEY = os.getenv("SINDRI_API_KEY", "")
+API_URL = os.getenv("SINDRI_API_URL", "https://forge.sindri.app/api/")
 
 circuit_name = "float_radius"
 circuit_upload_path = "circuit"
@@ -14,7 +15,7 @@ proof_input_file_path = "input.json"
 with open(proof_input_file_path, "r") as f:
     proof_input = f.read()
 
-sindri_sdk = SindriSdk(verbose_level=1, api_key=API_KEY)
+sindri_sdk = SindriSdk(verbose_level=1, api_key=API_KEY, api_url=API_URL)
 circuit_id = sindri_sdk.create_circuit(circuit_name, circuit_upload_path)
 proof_id = sindri_sdk.prove_circuit(circuit_id, proof_input)
 
