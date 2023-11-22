691f3b9a6be406b57649b167c045c4e3fce0a4ed
Katie McKeon <katiemckeonphd@gmail.com>
2023-11-22 17:58:36 +0000

    revisions to rust quickstart w/o tar

--- reference_code/quickstart_rust/src/main.rs
+++ reference_code/quickstart_rust/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    env,
+    option_env,
     fs::File,
     io::Read,
     time::Duration
@@ -7,7 +7,7 @@ use std::{
 use serde::Deserialize;
 use reqwest::{
     Client, 
-    header::HeaderMap,
+    header::{HeaderMap, HeaderValue},
     multipart::Part
 };
 
@@ -30,31 +30,26 @@ pub struct PollResponse {
 }
 
 // Functions which return Reqwest Header
-fn headers_json() -> HeaderMap {
+fn headers_json(api_key: &str) -> HeaderMap {
     let mut headers_json = HeaderMap::new();
     headers_json.insert("Accept", "application/json".parse().unwrap());
+    headers_json.insert("Authorization", HeaderValue::from_str(&format!("Bearer {api_key}").to_string()).unwrap());
     headers_json
 }
-fn headers_multipart() -> HeaderMap {
-    let mut headers_multipart = HeaderMap::new();
-    headers_multipart.insert("Accept", "multipart/form-data".parse().unwrap());
-    headers_multipart
-}
-
 
 // Polling loop while circuit compile or proof is in progress
 async fn poll_status(
     endpoint: String, 
-    api_url: &String,
-    api_key_querystring: &String,
+    api_url: &str,
+    api_key: &str,
     timeout: i64
 ) -> PollResponse {
     let client = Client::new();
     for i in 1..timeout {
 
         let response = client
-        .get(format!("{api_url}{endpoint}{api_key_querystring}"))
-        .headers(headers_json())
+        .get(format!("{api_url}{endpoint}"))
+        .headers(headers_json(api_key))
         .send()
         .await
         .unwrap();
@@ -77,59 +72,58 @@ async fn poll_status(
 #[tokio::main]
 async fn main() {
 
-    let api_key: String = env::var("SINDRI_API_KEY").unwrap();
-    let api_key_querystring: String = format!("?api_key={api_key}");
+    let api_key: &str = option_env!("SINDRI_API_KEY").unwrap_or("");
+    let api_url_prefix: &str = option_env!("SINDRI_API_URL").unwrap_or("https://forge.sindri.app/api/");
     
-    let api_version: &str = "v1";
-    let api_url: String = format!("https://forge.sindri.app/api/{api_version}/");
-    
+    let api_version: &str = "v1/";
+    let api_url: String = api_url_prefix.to_owned()  + api_version;
+
+    // Load the circuit .tar.gz file and create multipart form
+    let mut file = File::open("../../circuit_database/circom/multiplier2.tar.gz").unwrap();
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents).expect("Unable to read tar file");
+    let part = Part::bytes(contents).file_name("filename.filetype");
+    let upload = reqwest::multipart::Form::new().text("circuit_name","multiplier2").part("files", part);
+
     // Create new circuit
     println!("1. Creating circuit...");
     let client = Client::new();
     let mut response = client
-        .post(format!("{api_url}circuit/create{api_key_querystring}"))
-        .headers(headers_json())
-        .form(&[
-            ("circuit_name", "multiplier_example"),
-        ])
+        .post(format!("{api_url}circuit/create"))
+        .headers(headers_json(api_key))
+        .multipart(upload)
         .send()
         .await
         .unwrap();
     assert_eq!(&response.status().as_u16(), &201u16, "Expected status code 201");
     let circuit_id = response.json::<CircuitResponse>().await.unwrap().circuit_id; 
 
-    // Load the circuit .tar.gz file and create multipart form
-    let mut file = File::open("../circuit_database/circom/multiplier2").unwrap();
-    let mut contents = Vec::new();
-    file.read_to_end(&mut contents).expect("Unable to read tar file");
-    let part = Part::bytes(contents).file_name("filename.filetype");
-    let upload = reqwest::multipart::Form::new().part("files", part);
 
     // Upload the circuit file
-    let client = Client::new();
-    response = client
-    .post(format!("{api_url}circuit/{circuit_id}/uploadfiles{api_key_querystring}"))
-        .headers(headers_multipart())
-        .multipart(upload)
-        .send()
-        .await
-        .unwrap();
-    assert_eq!(&response.status().as_u16(), &201u16, "Expected status code 201");
+    // let client = Client::new();
+    // response = client
+    // .post(format!("{api_url}circuit/{circuit_id}/uploadfiles"))
+    //     .headers(headers_multipart())
+    //     .multipart(upload)
+    //     .send()
+    //     .await
+    //     .unwrap();
+    // assert_eq!(&response.status().as_u16(), &201u16, "Expected status code 201");
 
     // Initiate circuit compilation
-    response = client
-    .post(format!("{api_url}circuit/{circuit_id}/compile{api_key_querystring}"))
-    .headers(headers_json())
-    .send()
-    .await
-    .unwrap();
-    assert_eq!(&response.status().as_u16(), &201u16, "Expected status code 201");
+    // response = client
+    // .post(format!("{api_url}circuit/{circuit_id}/compile{api_key_querystring}"))
+    // .headers(headers_json())
+    // .send()
+    // .await
+    // .unwrap();
+    // assert_eq!(&response.status().as_u16(), &201u16, "Expected status code 201");
 
     // Poll circuit detail until it has a status of Ready or Failed
     let circuit_data = poll_status(
         format!("circuit/{circuit_id}/detail"),
         &api_url,
-        &api_key_querystring,
+        api_key,
         600).await;
     if circuit_data.status == "Failed" {
         println!("Circuit compilation failed.");
@@ -143,8 +137,8 @@ async fn main() {
     let map = serde_json::json!({"proof_input": proof_input});
 
     let response = client
-    .post(format!("{api_url}circuit/{circuit_id}/prove{api_key_querystring}"))
-    .headers(headers_json())
+    .post(format!("{api_url}circuit/{circuit_id}/prove"))
+    .headers(headers_json(api_key))
     .json(&map)
     .send()
     .await
@@ -157,7 +151,7 @@ async fn main() {
     let proof_data = poll_status(
         format!("proof/{proof_id}/detail"),
         &api_url,
-        &api_key_querystring,
+        api_key,
         600).await;
     if &proof_data.status == "Failed" {
         println!("Proving failed.");
