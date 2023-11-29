d0e6322899b78346167f47b35b405e15d90badcc
Karl Preisner <karl@sindri.app>
2023-11-29 18:32:28 +0000

    overhaul _prepare_circuit_database.py

--- circuit_database/README.md
+++ circuit_database/README.md
@@ -19,3 +19,49 @@ Circuit uploads to the website must be archived in a **gzip** tarfile (`.tar.gz`
 
 Within each circuit directory, we include a `.tar.gz` and a `.zip` of the circuit directory so downloading a circuit from GitHub and uploading it to Sindri's interactive website is simple.
 
+## `_prepare_circuit_database.py`
+**TL;DR: `python3 _prepare_circuit_database.py --compress`**
+
+**The `_prepare_circuit_database.py` script is a utility for compressing all sample circuits in `circuit_database/` to be easily used as file uploads in the interactive website.**
+- It ensures every sample circuit directory in one of the framework subdirectories is a valid circuit - it contains the necessary files for an upload to the Sindri API.
+- The script can be run from any directory. It uses relative paths from the file location (you do not have to worry about the paths being messed up because of your shell's current working directory).
+- All compressed objects for a circuit are stored in the circuit's parent directory and are gitignored (see `.gitignore`).
+- The script has no requirements other than `python3`.
+
+### Usage
+```bash
+$ python3 _prepare_circuit_database.py --help
+usage: _prepare_circuit_database.py [-h] [-c] [-r] [-d] [-q]
+
+options:
+  -h, --help      show this help message and exit
+  -c, --compress  Run compression (default: False)
+  -r, --remove    Remove compressed objects (default: False)
+  -d, --dry-run   Dry run (default: False)
+  -q, --quiet     Suppress verbose stdout (default: False)
+usage: _prepare_circuit_database.py [-h] [-c] [-r] [-d] [-q]
+
+options:
+  -h, --help      show this help message and exit
+  -c, --compress  Run compression (default: False)
+  -r, --remove    Remove compressed objects (default: False)
+  -d, --dry-run   Dry run (default: False)
+  -q, --quiet     Suppress verbose stdout (default: False)
+
+To run compression/removal, use the --compress OR the --remove flags:
+  python3 _prepare_circuit_database.py --compress
+  python3 _prepare_circuit_database.py --remove
+
+Add the --dry-run flag to perform a dry run:
+  python3 _prepare_circuit_database.py --compress --dry-run
+  python3 _prepare_circuit_database.py --remove --dry-run
+
+Add the --quiet flag to suppress verbose stdout:
+  python3 _prepare_circuit_database.py --compress --quiet
+```
+
+What happens if I run the compression script multiple times?
+- The script will overwrite existing compressed objects!
+
+What happens if I do not include either the `--compress` or `--remove` flags?
+- The script will prompt for `[y/n]` to continue with running compression (`--compress`).
--- circuit_database/_prepare_circuit_database.py
+++ circuit_database/_prepare_circuit_database.py
@@ -2,252 +2,237 @@
 import json
 import os
 import pathlib
-import shutil
 import sys
 import tarfile
 import zipfile
 from glob import glob
 
-"""
-This script is a utility for locally validating sample circuits and compressing them. 
-- It assumes every sample circuit directory in one of the framework subdirectories is a valid circuit. 
-- The script can be run from any directory. It uses relative paths from the file location (you do not have to worry about the paths being messed up because of your shell's current working directory).
-- All compressed circuit objects that are created by the `_prepare_circuit_database.py` script for a circuit are stored inside the circuit's directory.
-- The script has no requirements other than `python3`.
-- The resulting compressed circuits are gitignored (see `.gitignore`).
-
-**What happens if I run the script multiple times?**
-- The script will remove existing compressed objects before recompressing the directory, ***provided that the circuit directory was not renamed***!
-
-#### Dry Run
-`./_prepare_circuit_database.py --dry-run` option
-
-The **Dry Run** will perform a validity check on all of the supplied circuits directories. 
-- If a valid circuit directory does not have a `README.md` or a valid `sindri.json` file, it will exit with an error.
-
-#### Usage
-```
-$ python3 _prepare_circuit_database.py --help
-```
-
-If `--compress` or `--dry-run` are not supplied, the script will prompt for `y/n` to continue with running compression.
-- If `n` is supplied or `--dry-run` is specified, the script will only perform a **dry run**.
-"""
-
+global QUIET
+QUIET = False
 
 RELPATH_OF_THIS_DIRECTORY = os.path.relpath(pathlib.Path(__file__).parent.resolve())
 
-CIRCUIT_PARENT_DIRS_TO_BE_COMPRESSED = [
+CIRCUIT_PARENT_DIRS = [
     "circom/",
-    "gnark/",
-    "halo2/",
-    "noir/"
+    "gnark/v0.8.1/",
+    "gnark/v0.9.0/",
+    "halo2/axiom-v0.2.2/",
+    "halo2/axiom-v0.3.0/",
+    "noir/",
 ]
 
 
-MANUAL_DIRS_TO_BE_COMPRESSED = []
-# Deduplicate just in case
-MANUAL_DIRS_TO_BE_COMPRESSED = list(set(MANUAL_DIRS_TO_BE_COMPRESSED))
-MANUAL_DIRS_TO_BE_COMPRESSED.sort()
-
-
-
-def compress_circuit_dir(output_filename, source_dir, remove_only: bool = False):
+def compress_dirs(source_dirs: list[str] | list[pathlib.Path], dry_run: bool = False) -> None:
     """
-    1. Remove source_dir/output_filename.tar.gz, source_dir/output_filename.zip
-    2. Create output_filename.tar.gz, output_filename.zip from source_dir
-    3. Move output_filename.tar.gz, output_filename.zip into source_dir
+    For every `source_dir` in `source_dirs`, create/overwrite
+    `<source_dir>.tar.gz` and `<source_dir>.zip`.
+    ```txt
+    tar -zcvf <source_dir>.tar.gz source_dir
+    zip -r    <source_dir>.zip    source_dir
+    ```
     """
-    if not QUIET:
-        if not remove_only:
-            print(f"(Re)compressing circuit directory: {source_dir}")
-        else:
-            print(f"Removing circuit compressed objects: {source_dir}")
+    print(f"\nBEGIN: (Re)compressing {len(source_dirs)} circuit(s)\n")
+    for source_dir in source_dirs:
+        if not QUIET:
+            print(f"\nSource {str(source_dir)}")
 
-    final_tarfile_name = output_filename + ".tar.gz"
-    final_tarfile_destination = os.path.join(source_dir, final_tarfile_name)
-    final_zipfile_name = output_filename + ".zip"
-    final_zipfile_destination = os.path.join(source_dir, final_zipfile_name)
+        targz_file, zip_file = get_compressed_file_paths(source_dir)
 
-    try:
-        os.remove(final_tarfile_destination)
-    except FileNotFoundError:
-        pass
-    try:
-        os.remove(final_zipfile_destination)
-    except FileNotFoundError:
-        pass
+        if not dry_run:
+            with tarfile.open(targz_file, "w:gz") as tar:
+                tar.add(source_dir, arcname=targz_file.name)
+        if not QUIET:
+            print(f"       {targz_file}")
 
-    if remove_only:
-        return
-
-    with tarfile.open(final_tarfile_name, "w:gz") as tar:
-        tar.add(source_dir, arcname=output_filename)
-
-    with zipfile.ZipFile(final_zipfile_name, mode="w") as archive:
-        for file_path in pathlib.Path(source_dir).iterdir():
-            archive.write(file_path, arcname=file_path.name)
-
-    shutil.move(final_tarfile_name, final_tarfile_destination)
-    shutil.move(final_zipfile_name, final_zipfile_destination)
+        if not dry_run:
+            with zipfile.ZipFile(zip_file, mode="w") as archive:
+                for file_path in pathlib.Path(source_dir).iterdir():
+                    archive.write(file_path, arcname=file_path.name)
+        if not QUIET:
+            print(f"       {zip_file}")
+    print(f"\nSUCCESS. Compressed {len(source_dirs)} circuit(s).")
 
 
-def validate_circuit_directory(circuit_dir) -> None:
+def get_child_dirs_from_parent_dirs(circuit_parent_dirs: list[str]) -> list[str]:
     """
-    Exit if `circuit_dir`
-    - is missing a `README.md`
-    - is missing an `input.json` file
-    - is missing a `Sindri.json` or `sindri.json` file
-    - the `Sindri.json` file is a dictionary
-    - the `Sindri.json` file has no duplicate keys (case-insensitive)
-    - the `Sindri.json` field is missing the `circuit_type` key (case-insensitive).
-    """
-    if not QUIET:
-        print(f"VALIDATING: {circuit_dir}")
-
-    # Ensure the directory exists
-    if not os.path.exists(circuit_dir):
-        sys.exit(f"\nERROR: {circuit_dir} does not exist.")
-
-    # Ensure circuit_dir contains a README.md
-    readme_path = os.path.join(circuit_dir, "README.md")
-    if not os.path.exists(readme_path):
-        sys.exit(f"\nERROR: {circuit_dir} is missing a README.md file.")
-
-    # Ensure circuit_dir contains an input.json
-    # or Prover.toml (Noir circuits only)
-    input_path = os.path.join(circuit_dir, "input.json")
-    noir_input_path = os.path.join(circuit_dir, "Prover.toml")
-    if not os.path.exists(input_path) and not os.path.exists(noir_input_path):
-        sys.exit(f"\nERROR: {circuit_dir} is missing an input.json or Prover.toml file.")
-
-    # Ensure circuit_dir contains a Sindri.json or sindri.json
-    sindri_json_path = os.path.join(circuit_dir, "Sindri.json")
-    if not os.path.exists(sindri_json_path):
-        sindri_json_path = os.path.join(circuit_dir, "sindri.json")
-        if not os.path.exists(str(sindri_json_path).lower()):
-            sys.exit(f"\nERROR: {circuit_dir} is missing a Sindri.json file.")
-
-    # Load the contents of the Sindri.json
-    sindri_json = {}
-    with open(sindri_json_path, "r") as f:
-        sindri_json = json.load(f)
-    if not isinstance(sindri_json, dict):
-        sys.exit(f"\nERROR: {circuit_dir} Sindri.json is not a dictionary.")
-
-    sindri_json_keys_lower = [key.lower() for key in sindri_json.keys()]
-
-    # Ensure there are no duplicate Sindri.json keys (case-insensitive)
-    if len(sindri_json_keys_lower) != len(set(sindri_json_keys_lower)):
-        sys.exit(f"\nERROR: {sindri_json_path} has duplicate keys (case-insensitive).")
-
-    if "circuit_type" not in sindri_json_keys_lower:
-        sys.exit(f"\nERROR: {sindri_json_path} is missing the `circuit_type` key.")
-
-
-def get_valid_circuit_dirs_from_parent_dir(parent_dir) -> list[str]:
-    """
-    Look at all child dirs of parent_dir. If the child dir is a valid circuit,
-    add it to the list of returned dirs that should be compressed.
-    """
-    valid_circuit_dirs = []
-
-    child_dirs = glob(os.path.join(parent_dir, "*", ""))
-    child_dirs.sort()
-    for child_dir in child_dirs:
-        validate_circuit_directory(child_dir)
-        valid_circuit_dirs.append(child_dir)
-    valid_circuit_dirs.sort()
-    return valid_circuit_dirs
-
-
-def get_circuit_dirs_from_parent_dir(parent_dir) -> list[str]:
-    """
-    Return all child dirs of parent_dir.
+    Return all child dirs of every parent dir in `circuit_parent_dirs`.
+    Transform them to relative paths based on the current working directory.
     """
     circuit_dirs = []
-    for circuit_dir in glob(os.path.join(parent_dir, "*", "")):
-        circuit_dirs.append(os.path.relpath(circuit_dir))
+    for parent_dir in circuit_parent_dirs:
+        for circuit_dir in glob(os.path.join(RELPATH_OF_THIS_DIRECTORY, parent_dir, "*", "")):
+            circuit_dirs.append(os.path.relpath(circuit_dir))
     circuit_dirs.sort()
     return circuit_dirs
 
 
-global QUIET
-QUIET = False
+def get_compressed_file_paths(source_dir: str | pathlib.Path) -> list[pathlib.Path]:
+    """
+    Return the relative file paths (based on current working directory)
+    to the compressed files for `source_dir`.
+    ```
+    # Input
+    /path_to/source_dir/
+    # Output
+    /path_to/source_dir.tar.gz
+    /path_to/source_dir.zip
+    ```
+    """
+    source_dir = pathlib.Path(os.path.relpath(source_dir))
+    if not source_dir.is_dir():
+        raise ValueError("source_dir must be a directory")
+    return [source_dir.with_suffix(".tar.gz"), source_dir.with_suffix(".zip")]
+
+
+def remove_compressed_artifacts(
+    source_dirs: list[str] | list[pathlib.Path], dry_run: bool = False
+) -> None:
+    """
+    For every `source_dir` in `source_dirs`, remove `<source_dir>.tar.gz` and `<source_dir>.zip`.
+    ```txt
+    rm -f <source_dir>.tar.gz
+    rm -f <source_dir>.zip
+    ```
+    """
+    print(f"\nBEGIN: Removing {len(source_dirs)} circuit(s) artifacts\n")
+    num_removed = 0
+    for source_dir in source_dirs:
+        file_paths = get_compressed_file_paths(source_dir)
+        for file_path in file_paths:
+            if os.path.exists(file_path):
+                if not dry_run:
+                    os.remove(file_path)
+                num_removed += 1
+                if not QUIET:
+                    print(f"Removed {file_path}")
+    print(f"\nSUCCESS. Removed {num_removed} artifacts for {len(source_dirs)} circuit(s).")
+
+
+def validate_circuit_dirs(circuit_dirs: list[str] | list[pathlib.Path]) -> None:
+    """
+    Ensure that every circuit_dir in `circuit_dirs` is a valid circuit.
+
+    Exit if `circuit_dir`
+    - is missing a `README.md`
+    - is missing an example input file
+    - is missing a `Sindri.json` or `sindri.json` file
+    - the `Sindri.json` file is not a dictionary
+    - the `Sindri.json` file has no duplicate keys (case-insensitive)
+    - the `Sindri.json` field is missing the `circuit_type` key (case-insensitive).
+    - the `Sindri.json` field is missing the `name` key (case-insensitive).
+
+    TODO: Use sindri_manifest JSON schema from Sindri API for validation for proper validation.
+    """
+    for circuit_dir in circuit_dirs:
+        if not QUIET:
+            print(f"VALIDATING: {circuit_dir}")
+
+        # Ensure the directory exists
+        if not os.path.exists(circuit_dir):
+            sys.exit(f"\nERROR: {circuit_dir} does not exist.")
+
+        # Ensure circuit_dir contains a README.md
+        readme_path = os.path.join(circuit_dir, "README.md")
+        if not os.path.exists(readme_path):
+            sys.exit(f"\nERROR: {circuit_dir} is missing a README.md file.")
+
+        # Ensure circuit_dir contains an input.json
+        # or Prover.toml (Noir circuits only)
+        input_path = os.path.join(circuit_dir, "input.json")
+        noir_input_path = os.path.join(circuit_dir, "Prover.toml")
+        if not os.path.exists(input_path) and not os.path.exists(noir_input_path):
+            sys.exit(f"\nERROR: {circuit_dir} is missing an input.json or Prover.toml file.")
+
+        # Ensure circuit_dir contains a Sindri.json or sindri.json
+        sindri_json_path = os.path.join(circuit_dir, "Sindri.json")
+        if not os.path.exists(sindri_json_path):
+            sindri_json_path = os.path.join(circuit_dir, "sindri.json")
+            if not os.path.exists(str(sindri_json_path).lower()):
+                sys.exit(f"\nERROR: {circuit_dir} is missing a Sindri.json file.")
+
+        # Load the contents of the Sindri.json
+        sindri_json = {}
+        with open(sindri_json_path, "r") as f:
+            sindri_json = json.load(f)
+        if not isinstance(sindri_json, dict):
+            sys.exit(f"\nERROR: {circuit_dir} Sindri.json is not a dictionary.")
+
+        sindri_json_keys_lower = [key.lower() for key in sindri_json.keys()]
+
+        # Ensure there are no duplicate Sindri.json keys (case-insensitive)
+        if len(sindri_json_keys_lower) != len(set(sindri_json_keys_lower)):
+            sys.exit(f"\nERROR: {sindri_json_path} has duplicate keys (case-insensitive).")
+
+        # Check required keys for all circuit types
+        if "circuit_type" not in sindri_json_keys_lower:
+            sys.exit(f"\nERROR: {sindri_json_path} is missing the `circuit_type` key.")
+        if "name" not in sindri_json_keys_lower:
+            sys.exit(f"\nERROR: {sindri_json_path} is missing the `name` key.")
+
+
+def _print_help_message() -> None:
+    print("\nTo run compression/removal, use the --compress OR the --remove flags:")
+    print("  python3 _prepare_circuit_database.py --compress")
+    print("  python3 _prepare_circuit_database.py --remove")
+    print("\nAdd the --dry-run flag to perform a dry run:")
+    print("  python3 _prepare_circuit_database.py --compress --dry-run")
+    print("  python3 _prepare_circuit_database.py --remove --dry-run")
+    print("\nAdd the --quiet flag to suppress verbose stdout:")
+    print("  python3 _prepare_circuit_database.py --compress --quiet")
+    print()
+
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-        "-c", "--compress", action="store_true", help="Run compression (omit for dry run)"
-    )
-    parser.add_argument(
-        "-r",
-        "--remove-only",
-        action="store_true",
-        help="Only remove compressed objects (requires -c)",
-    )
-    parser.add_argument("-d", "--dry-run", action="store_true", help="Run dry run (validate files)")
-    parser.add_argument("-q", "--quiet", action="store_true", help="Quiet: Suppress stdout")
-    args = parser.parse_args()
+    parser.add_argument("-c", "--compress", action="store_true", help="Run compression")
+    parser.add_argument("-r", "--remove", action="store_true", help="Remove compressed objects")
+    parser.add_argument("-d", "--dry-run", action="store_true", help="Dry run")
+    parser.add_argument("-q", "--quiet", action="store_true", help="Suppress verbose stdout")
+    try:
+        args = parser.parse_args()
+    except:  # NOTE: This does not work as intended without bare except.
+        parser.print_help()
+        _print_help_message()
+        sys.exit(0)
+
     if args.quiet:
         QUIET = True
 
-    remove_only = args.remove_only
+    dry_run = args.dry_run
     compress = args.compress
-    if not args.dry_run and not compress:
-        if remove_only:
-            prompt = input("Run removal? [y/n]")
-        else:
-            prompt = input("Run compression? [y/n]")
+    remove = args.remove
+    if dry_run:
+        print("\n***DRY RUN BEGIN***\n")
+    if not compress and not remove:
+        print("No command specified.")
+        prompt = input("\nContinue and run compression? (--compress)\n[y/n]\n")
         if prompt.lower() == "y":
             compress = True
-
-    if not compress:
-        print("PERFORMING DRY RUN. Run with '-c' or '--compress' to run compression.")
+        else:
+            sys.exit("Goodbye.")
 
     if not QUIET:
-        print("\nThis script compresses all circuit folders using multiple methods (.tar.gz, .zip)")
-        print("and places the compressed results inside their respective circuit folder.\n")
+        print(
+            "\nThis script compresses all circuit folders using"
+            " multiple methods (.tar.gz, .zip) and places the compressed"
+            " artifacts in the parent folder of the circuit folder.\n"
+        )
 
-    circuit_dirs_to_compress = []
+    # Obtain circuit_dirs from CIRCUIT_PARENT_DIRS
+    circuit_dirs = get_child_dirs_from_parent_dirs(CIRCUIT_PARENT_DIRS)
 
-    # Detect and add circuit dirs from CIRCUIT_PARENT_DIRS_TO_BE_COMPRESSED
-    for parent_dir in CIRCUIT_PARENT_DIRS_TO_BE_COMPRESSED:
-        parent_dir = os.path.join(RELPATH_OF_THIS_DIRECTORY, parent_dir)
-        circuit_dirs_to_compress += get_circuit_dirs_from_parent_dir(parent_dir)
+    # Validate all circuit_dirs
+    validate_circuit_dirs(circuit_dirs)
 
-    # Add circuit dirs from MANUAL_DIRS_TO_BE_COMPRESSED
-    for circuit_dir in MANUAL_DIRS_TO_BE_COMPRESSED:
-        circuit_dir = os.path.join(RELPATH_OF_THIS_DIRECTORY, circuit_dir)
-        circuit_dirs_to_compress.append(circuit_dir)
-
-    # Deduplicate and sort
-    circuit_dirs_to_compress = list(set(circuit_dirs_to_compress))
-    circuit_dirs_to_compress.sort()
-
-    num_circuits = len(circuit_dirs_to_compress)
-
-    # Validate all circuit_dirs_to_compress
-    for circuit_dir in circuit_dirs_to_compress:
-        validate_circuit_directory(circuit_dir)
-
-    if compress:
-        # Compress all circuit_dirs_to_compress
+    if remove:
+        # Remove compressed artifacts for given circuit_dirs
+        remove_compressed_artifacts(circuit_dirs, dry_run=dry_run)
+    elif compress:
+        # Compress all circuit_dirs
+        compress_dirs(circuit_dirs, dry_run=dry_run)
+    if dry_run:
+        print("\n***DRY RUN SUCCESS***")
         if not QUIET:
-            print(f"\nBEGIN: {num_circuits} circuit(s)\n")
-        for circuit_dir in circuit_dirs_to_compress:
-            last_dir_name = pathlib.PurePath(circuit_dir).name
-            compress_circuit_dir(last_dir_name, circuit_dir, remove_only=remove_only)
-
-        if not QUIET:
-            print(f"\nSUCCESS. {num_circuits} valid circuit(s)")
-        else:
-            print(f"SUCCESS. {num_circuits} valid circuit(s)")
-    else:
-        if not QUIET:
-            print(f"\nDRY RUN SUCCESS. {num_circuits} valid circuit(s)")
-            print("Run with '-c' or '--compress' to run compression.\n")
-        else:
-            print(f"DRY RUN SUCCESS. {num_circuits} valid circuit(s)")
+            _print_help_message()
