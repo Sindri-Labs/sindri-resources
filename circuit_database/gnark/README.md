35404743d0453f960be5cf68266e1da802dbe8c8
Katie McKeon <62887234+katiemckeon@users.noreply.github.com>
2024-05-08 09:05:43 -0500

    Add MTP Gnark+Plonk Example (#85)
    This PR introduces a distinct Gnark circuit that verifies merkle tree
    proofs. While the circuit is compatible with either proving backend, we
    specifically added the new `plonk` backend in the circuit's Sindri
    manifest to demonstrate the new compatibility.
    
    ## Testing Instructions
    
    1. Upload one of the compressed files to the frontend (double-checking
    my compression)
    2. Locally, change directories to the new circuit. Then do `sindri
    deploy; sindri proof create`. Thus testing the JSON input file and
    circuit functionality.
    3. Inspect the readme additions.

--- circuit_database/gnark/README.md
+++ circuit_database/gnark/README.md
@@ -8,7 +8,9 @@
 | exponentiate | ? | [Gnark Github Examples](https://github.com/Consensys/gnark/blob/master/examples/exponentiate/exponentiate.go) | Checks that $y == x**e$ where $e$ is private |
 
 ### Gnark v0.9.0 (Gnark-Crypto v0.11.2)
-| Name | Size (DEGREE) | Original Source | Functionality | 
-| - | - | - | - | 
-| cubic bn254 | 3 | [Gnark Github Examples](https://github.com/Consensys/gnark/blob/master/examples/cubic/cubic.go) | Checks $x^3 + x + 5 == y$ for public $y$ and private $x$ |
-| poseidon | 214 | [Vocdoni's Gnark Primitives](https://github.com/vocdoni/gnark-crypto-primitives/tree/main) | Computes the Poseidon hash of a preimage  |
+| Name | Size (DEGREE) | Proving Scheme |  Original Source | Functionality | 
+| - | - | - | - | - | 
+| cubic bn254 | 3 | Groth16 | [Gnark Github Examples](https://github.com/Consensys/gnark/blob/master/examples/cubic/cubic.go) | Checks $x^3 + x + 5 == y$ for public $y$ and private $x$ |
+| poseidon | 214 | Groth16 | [Vocdoni's Gnark Primitives](https://github.com/vocdoni/gnark-crypto-primitives/tree/main) | Computes the Poseidon hash of a preimage  |
+| merkle_tree | 3647 | Plonk | [Hashcloak's Merkle Tree Library](https://github.com/hashcloak/merkle_trees_gnark/tree/master?tab=readme-ov-file) | Verifies a merkle tree proof of depth 5 | 
+
---
+++ circuit_database/gnark/v0.9.0/merkle_tree/mtp.go
@@ -0,0 +1,113 @@
+package mtp
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/witness"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/hash/mimc"
+)
+
+// Circuit that checks a Merkle Proof
+// This is a variation of the existing circuit in the gnark library https://github.com/Consensys/gnark/blob/master/std/accumulator/merkle/verify.go
+
+type Circuit struct {
+	Root          frontend.Variable    `gnark:",public"`
+	ProofElements [5]frontend.Variable // private
+	ProofIndex    frontend.Variable    // private
+	Leaf          frontend.Variable    // private
+}
+
+func (circuit *Circuit) Define(api frontend.API) error {
+	h, err := mimc.NewMiMC(api)
+	if err != nil {
+		return err
+	}
+
+	// Hash leaf
+	h.Reset()
+	h.Write(circuit.Leaf)
+	hashed := h.Sum()
+
+	depth := len(circuit.ProofElements)
+	proofIndices := api.ToBinary(circuit.ProofIndex, depth)
+
+	// Continuously hash with the proof elements
+	for i := 0; i < len(circuit.ProofElements); i++ {
+		element := circuit.ProofElements[i]
+		// 0 = left, 1 = right
+		index := proofIndices[i]
+
+		d1 := api.Select(index, element, hashed)
+		d2 := api.Select(index, hashed, element)
+
+		h.Reset()
+		h.Write(d1, d2)
+		hashed = h.Sum()
+	}
+
+	// Verify calculates hash is equal to root
+	api.AssertIsEqual(hashed, circuit.Root)
+	return nil
+}
+
+// Common utility for reading JSON in from a file.
+func ReadFromInputPath(pathInput string) (map[string]interface{}, error) {
+
+	absPath, err := filepath.Abs(pathInput)
+	if err != nil {
+		fmt.Println("Error constructing absolute path:", err)
+		return nil, err
+	}
+
+	file, err := os.Open(absPath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var data map[string]interface{}
+	err = json.NewDecoder(file).Decode(&data)
+	if err != nil {
+		panic(err)
+	}
+
+	return data, nil
+}
+
+// Construct a witness from input data in a JSON file.
+func FromJson(pathInput string) witness.Witness {
+
+	data, err := ReadFromInputPath(pathInput)
+	if err != nil {
+		panic(err)
+	}
+
+	root, _ := new(big.Int).SetString(data["Root"].(string), 10)
+	proofIndex, _ := new(big.Int).SetString(data["ProofIndex"].(string), 10)
+	leaf, _ := new(big.Int).SetString(data["Leaf"].(string), 10)
+	str := strings.Trim(data["ProofElements"].(string), "[]")
+	strValues := strings.Split(str, ",")
+	bigIntList := [5]frontend.Variable{}
+	for i, v := range strValues {
+		num, _ := new(big.Int).SetString(strings.TrimSpace(v), 10)
+		bigIntList[i] = num
+	}
+	assignment := Circuit{
+		Root:          root,
+		ProofElements: bigIntList,
+		ProofIndex:    proofIndex,
+		Leaf:          leaf,
+	}
+	w, err := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
+	if err != nil {
+		panic(err)
+	}
+	return w
+}
