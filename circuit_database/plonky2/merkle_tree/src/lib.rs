8f4f48c62df0bef2ddb86b58bd18a3e4567856ed
Roy Rotstein <roy.rotstein87@gmail.com>
2024-08-08 01:11:07 -0700

    refactored merkle tree circuit

--- circuit_database/plonky2/merkle_tree/src/lib.rs
+++ circuit_database/plonky2/merkle_tree/src/lib.rs
@@ -1,26 +1,51 @@
-pub mod merkle_tree;
-
-use crate::merkle_tree::MerkleTree;
-
-use plonky2::field::types::Field;
-use plonky2::hash::hash_types::{HashOut, RichField};
-use plonky2::hash::poseidon::PoseidonHash;
-use plonky2::iop::witness::{PartialWitness, WitnessWrite};
-use plonky2::plonk::circuit_builder::CircuitBuilder;
-use plonky2::plonk::circuit_data::{CircuitConfig, CommonCircuitData, VerifierOnlyCircuitData};
 use plonky2::plonk::proof::ProofWithPublicInputs;
 use plonky2::{
-    iop::target::Target,
-    plonk::config::{GenericConfig, Hasher, PoseidonGoldilocksConfig},
+    field::{goldilocks_field::GoldilocksField, types::Field},
+    hash::{
+        hash_types::{HashOut, HashOutTarget, RichField},
+        poseidon::PoseidonHash,
+    },
+    iop::witness::{PartialWitness, WitnessWrite},
+    plonk::{
+        circuit_builder::CircuitBuilder,
+        circuit_data::{CircuitConfig, CircuitData, CommonCircuitData, VerifierOnlyCircuitData},
+        config::{GenericConfig, PoseidonGoldilocksConfig},
+        proof,
+    },
 };
+
+mod merkle_tree;
+use crate::merkle_tree::MerkleTree;
 
 use serde::{Deserialize, Serialize};
 use std::fs;
 
 pub const D: usize = 2;
-
 pub type C = PoseidonGoldilocksConfig;
 pub type F = <C as GenericConfig<D>>::F;
+
+#[derive(Serialize, Deserialize)]
+pub struct InputData {
+    pub inputs: Vec<u64>,
+    pub index: usize,
+}
+impl InputData {
+    pub fn from_json(path: &str) -> Self {
+        let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
+        serde_json::from_str(&contents).unwrap()
+    }
+    pub fn set_leaf_index(&mut self, index: usize) {
+        // make sure that the user doesn't select an index that is out of bounds
+        assert!(index < self.inputs.len());
+        self.index = index;
+    }
+
+    pub fn get_layers(&self) -> usize {
+        let total_leaves = self.inputs.len();
+        let layers = ((total_leaves as f32).log2()) as usize;
+        layers
+    }
+}
 
 pub struct MerkleTreeCircuit {
     pub proof: ProofWithPublicInputs<F, C, D>,
@@ -28,111 +53,107 @@ pub struct MerkleTreeCircuit {
     pub common: CommonCircuitData<F, D>,
 }
 
-//user needs to specify the correct input type.  Should match the type in the from_json function
-#[derive(Serialize, Deserialize)]
-pub struct InputData {
-    pub inputs: Vec<u64>,
-}
-
 impl MerkleTreeCircuit {
     pub fn prove(path: &str) -> Self {
-        let config = CircuitConfig::standard_recursion_config();
-        let mut builder = CircuitBuilder::<F, D>::new(config);
+        let input_data = InputData::from_json(path);
+        let total_leaves = input_data.inputs.len();
+        let nr_layers = input_data.get_layers();
+        
+        // create the merkle tree
+        let mut leaves: Vec<F> = Vec::new();
+        for i in 0..total_leaves {
+          let leaf = F::from_canonical_u64(input_data.inputs[i] as u64);
+          leaves.push(leaf);
+        }
+        let tree: MerkleTree = MerkleTree::build(leaves.clone());
+        
+        // choose the leaf to prove in the merkle inclusion proof by selecting its index in the base layer of the merkle tree
+        let prove_leaf_index = input_data.index;
 
-        // The arithmetic circuit.
-        // The leaf to prove is in the Merkle Tree
-        let leaf_to_prove = builder.add_virtual_hash();
+        // create the merkle proof
+        let merkle_proof = tree.clone().get_merkle_proof(prove_leaf_index);
+        println!("Merkle proof: {:?}", merkle_proof.clone());
 
-        // The elements of the proof
-        let merkle_proof_elm_0 = builder.add_virtual_hash();
-        let merkle_proof_elm_1 = builder.add_virtual_hash();
+        let (circuit_data, targets) = verify_merkle_proof_circuit(prove_leaf_index, nr_layers);
+        
 
-        let level1_hash: plonky2::hash::hash_types::HashOutTarget = builder
-            .hash_or_noop::<PoseidonHash>(
-                [
-                    leaf_to_prove.elements.to_vec(),
-                    merkle_proof_elm_0.elements.to_vec(),
-                ]
-                .concat(),
-            );
-
-        // This is how we set the constraint of the expected root wrt the computed value
-        let expected_root = builder.hash_or_noop::<PoseidonHash>(
-            [
-                level1_hash.elements.to_vec(),
-                merkle_proof_elm_1.elements.to_vec(),
-            ]
-            .concat(),
-        );
-
-        // Registering a hash target actually registers 4 target elements
-        builder.register_public_inputs(&leaf_to_prove.elements);
-        builder.register_public_inputs(&merkle_proof_elm_0.elements);
-        builder.register_public_inputs(&merkle_proof_elm_1.elements);
-        builder.register_public_inputs(&expected_root.elements);
-
-        let data = builder.build::<C>();
-
-        // We import the input data and format it to the field element type
-        let input_data = from_json(path);
-        let input_len = input_data.len();
-        let leaves: Vec<F> = input_data
-            .clone()
-            .into_iter()
-            .map(F::from_canonical_u64)
-            .collect();
-
-        // Create the partial witness
+        // create the partial witness
         let mut pw = PartialWitness::new();
 
-        let tree: MerkleTree  = MerkleTree::build(leaves.clone());
-
-        let merkle_proof_leaf0 = tree.clone().get_merkle_proof(0);
-
-        // Obtain the target elements
-        let expected_public_inputs = data.prover_only.public_inputs.clone();
-
-        // Set first hash; the leaf we're trying to prove membership of
-        let leaf_to_prove = PoseidonHash::hash_or_noop(&[leaves[0]]);
-
-        for i in 0..input_len {
-            pw.set_target(expected_public_inputs[i], leaf_to_prove.elements[i]);
+        pw.set_hash_target(targets[0], tree.tree[0][prove_leaf_index]);
+        for i in 0..nr_layers {
+            pw.set_hash_target(targets[i+1], merkle_proof[i]);
         }
 
-        // Set first elm of proof
-        for i in 0..input_len {
-            pw.set_target(
-                expected_public_inputs[i + 4],
-                merkle_proof_leaf0[0].elements[i],
-            );
+        // public input: root of merkle tree
+        let expected_public_inputs = circuit_data.prover_only.public_inputs.clone();
+        for i in 0..4 {
+            pw.set_target(expected_public_inputs[i], tree.root.elements[i]);
         }
+        
+        // prove the circuit
+        let proof_with_pis = circuit_data.prove(pw).unwrap();
 
-        // Set second elm of proof
-        for i in 0..input_len {
-            pw.set_target(
-                expected_public_inputs[i + 8],
-                merkle_proof_leaf0[1].elements[i],
-            );
-        }
-
-        // Set root
-        for i in 0..input_len {
-            pw.set_target(expected_public_inputs[i + 12], tree.root.elements[i]);
-        }
-
-        let proof_with_pis = data.prove(pw).unwrap();
+        let verified = circuit_data.verify(proof_with_pis.clone()).unwrap();
+        println!("Verified: {:?}", verified);
 
         Self {
             proof: proof_with_pis,
-            verifier_only: data.verifier_only,
-            common: data.common,
-        }
+            verifier_only: circuit_data.verifier_only,
+            common: circuit_data.common,
+        } 
     }
 }
 
-pub fn from_json(path: &str) -> Vec<u64> {
-    let inputs = fs::read_to_string(path).unwrap();
-    let data: InputData = serde_json::from_str(&inputs).unwrap();
+pub fn verify_merkle_proof_circuit(leaf_index: usize, nr_layers: usize) -> (CircuitData<plonky2::field::goldilocks_field::GoldilocksField, PoseidonGoldilocksConfig, 2>, Vec<HashOutTarget>) {
+  let mut targets: Vec<plonky2::hash::hash_types::HashOutTarget> = Vec::new();
+  
+  let config = CircuitConfig::standard_recursion_config();
+  let mut builder: CircuitBuilder<plonky2::field::goldilocks_field::GoldilocksField, 2> = CircuitBuilder::<F, D>::new(config);
+  
+  // The leaf to prove is in the Merkle Tree
+  let leaf_to_prove = builder.add_virtual_hash();
+  targets.push(leaf_to_prove);
 
-    data.inputs
+  // The first hashing outside of the loop, since it uses the leaf_to_prove
+  let merkle_proof_elm= builder.add_virtual_hash();
+  targets.push(merkle_proof_elm);
+
+  let mut next_hash: plonky2::hash::hash_types::HashOutTarget;
+  if leaf_index % 2 == 0 {
+    next_hash = builder.hash_or_noop::<PoseidonHash>([
+      leaf_to_prove.elements.to_vec(), 
+      merkle_proof_elm.elements.to_vec()
+    ].concat());
+  } else {
+    next_hash = builder.hash_or_noop::<PoseidonHash>([
+      merkle_proof_elm.elements.to_vec(),
+      leaf_to_prove.elements.to_vec()
+    ].concat());
+  }
+
+  let mut current_layer_index = leaf_index / 2;
+  
+  for _layer in 1..nr_layers {
+    let merkle_proof_elm= builder.add_virtual_hash();
+    targets.push(merkle_proof_elm);
+
+    if current_layer_index % 2 == 0 {
+      next_hash = builder.hash_or_noop::<PoseidonHash>([
+        next_hash.elements.to_vec(), 
+        merkle_proof_elm.elements.to_vec()
+      ].concat());
+    } else {
+      next_hash = builder.hash_or_noop::<PoseidonHash>([
+        merkle_proof_elm.elements.to_vec(),
+        next_hash.elements.to_vec()
+      ].concat());
+    }
+    current_layer_index = current_layer_index/2;
+  }
+  // This is the expected root value
+  builder.register_public_inputs(&next_hash.elements);
+
+  let data = builder.build::<C>();
+  (data, targets)
 }
